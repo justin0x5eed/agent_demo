5c17c94d888fef222b177349a30fa0cdb0b414b2
Justin <justin0x5eed@gmail.com>
2025-11-16 18:19:02 +0800

    answer now

--- backend/content/views.py
+++ backend/content/views.py
@@ -12,19 +12,13 @@ def receive_message(request):
 
     base_url = "http://192.168.50.17:11434"
 
-    MODELS = [
-        ("qwen3", "qwen3:30b"),
-        ("gemma3", "gemma3:27b"),
-        ("gpt-oss", "gpt-oss:20b"),
-    ]
-    
-    # 修改下方索引以选择不同模型
-    SELECTED_MODEL_INDEX = 0
+    MODELS = {
+            "qwen3": "qwen3:30b",
+            "gemma3": "gemma3:27b",
+            "gpt-oss": "gpt-oss:20b",
+    }
     
     
-    model_key, model_name = MODELS[SELECTED_MODEL_INDEX]
-    
-    llm = OllamaLLM(model=model_name, base_url=base_url)
     
 
 
@@ -32,10 +26,15 @@ def receive_message(request):
     if not data:
         return Response({"detail": "No data provided."}, status=400)
 
-    print(f"Frontend payload: {data}")
-    answer = llm.invoke(question)
+    model_name = MODELS[data["model"]]
+    question = data["message"]
+    
+#    llm = OllamaLLM(model=model_name, base_url=base_url)
 
-    return Response({"status": "received", "data": data})
+    print(f"Frontend payload: {data}")
+#    answer = llm.invoke(question)
+
+    return Response({"status": "received", "data": [model_name, question]})
 
 
 
